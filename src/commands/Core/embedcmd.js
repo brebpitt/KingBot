f6993a3eb5484e1ca1f5f7ab7d4a0a6f8f56a508
brebpitt <egorvgeitupoi@gmail.com>
2026-07-21 22:34:54 +0700

    Update embedcmd.js

--- src/commands/Core/embedcmd.js
+++ src/commands/Core/embedcmd.js
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder, EmbedBuilder, ChannelType } = from 'discord.js';
 
-module.exports = {
+export default {
     data: new SlashCommandBuilder()
         .setName('эмбед')
         .setDescription('Отправить красивое эмбед-сообщение')
